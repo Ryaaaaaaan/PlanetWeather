0e2489c886802883bc9e480b681c31228f7289cc
Buska P <abdelrahim.baaziz@epitech.eu>
2026-01-01 07:24:59 +0100

    chore: cleanup - remove 15 unused/duplicate files

--- PlanetWeather/Reality/AtmosphericLimbView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-// Placeholder for missing file to satisfy build system
-struct AtmosphericLimbView: View {
-    var body: some View {
-        EmptyView()
-    }
-}
--- PlanetWeather/Views/Components/LiquidWeatherCard.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-// Placeholder for missing file to satisfy build system - Deprecated
-struct LiquidWeatherCard: View {
-    var body: some View {
-        EmptyView()
-    }
-}
